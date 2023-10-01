a875aff6ea1a1cc78ed7d408790098693c9406bf
Joel Vinay Kumar <lavotivinay@gmail.com>
2023-10-01 14:53:25 +0530

    add target blank to social

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -4,10 +4,10 @@ import { BsGithub, BsLinkedin } from 'react-icons/bs';
 const SocialMedia = () => (
   <div className="app__social">
     <div>
-      <a rel="noreferrer" role="button" aria-label="Link taking to github" href="https://github.com/joelvinaykumar"><BsGithub /></a>
+      <a rel="noreferrer" role="button" target="_blank" aria-label="Link taking to github" href="https://github.com/joelvinaykumar"><BsGithub /></a>
     </div>
     <div>
-      <a rel="noreferrer" role="button" aria-label="Link taking to linkedin" href="https://linkedin.com/in/joelvinaykumar"><BsLinkedin /></a>
+      <a rel="noreferrer" role="button" target="_blank" aria-label="Link taking to linkedin" href="https://linkedin.com/in/joelvinaykumar"><BsLinkedin /></a>
     </div>
   </div>
 );
